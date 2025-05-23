6e51e49a4e4ad6a5af027f45761f506d95ba3e8a
Syan <torressyanvale@gmail.com>
2025-05-23 14:04:03 +0800

    re

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
